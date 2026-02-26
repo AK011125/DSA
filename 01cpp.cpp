5dee203288a4ac26cecc0c195aec87974f9e8e01
ak011125 <ajaykumarjakhar08@gmail.com>
2026-02-26 09:31:10 +0530

    Initial

---
+++ 01cpp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    cout<<sqrt(4);
+    cout<<endl;
+    cout<<"Hello World";
+    cout<<endl;
+    cout<<"OGGY & NISHI";
+    return 0;
+}
